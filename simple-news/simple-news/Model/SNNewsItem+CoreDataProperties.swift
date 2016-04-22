c55e2fee94a7958e6f1a09aea1cfe06472a9d7b8
Aleksandr Ivashchenko <al.vl.ivashchenko@gmail.com>
2016-04-22 12:05:27 +0300

    Created News Item Model object, created class for network request

---
+++ simple-news/simple-news/Model/SNNewsItem+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  SNNewsItem+CoreDataProperties.swift
+//  simple-news
+//
+//  Created by Aleksandr Ivashchenko on 4/22/16.
+//  Copyright © 2016 Aleksandr Ivashchenko. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension SNNewsItem {
+
+    @NSManaged var title: String?
+    @NSManaged var link: String?
+    @NSManaged var pubDate: NSTimeInterval
+    @NSManaged var itemDescription: String?
+    @NSManaged var photoID: Int32
+
+}
---
+++ simple-news/simple-news/Model/SNNewsItem.swift
@@ -0,0 +1,17 @@
+//
+//  SNNewsItem.swift
+//  simple-news
+//
+//  Created by Aleksandr Ivashchenko on 4/22/16.
+//  Copyright © 2016 Aleksandr Ivashchenko. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class SNNewsItem: NSManagedObject {
+
+    
+
+}
